fb253276b6ad1db41be49be343cf8cab25fbb111
MaazMaahi <nehalmaaz@gmail.com>
2023-05-03 14:12:52 +0530

    Bssic Ui init

--- Netflix Clone/controllers/DownloadsViewController.swift
+++ Netflix Clone/controllers/DownloadsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DownloadsViewController.swift
 //  Netflix Clone
 //
 //  Created by Md Maaz Ahmad on 02/05/23.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DownloadsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemBackground
     }
-
-
 }
-
---
+++ Netflix Clone/controllers/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Md Maaz Ahmad on 02/05/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
